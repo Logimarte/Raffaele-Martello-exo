3e1d26e2de5149f01245aa1518d7515bc3f5ed95
Logi <LogitekAlfa@outlook.be>
2024-12-06 12:58:49 +0100

    Cache Online First

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,9 @@
 const version = 1.01
-const cacheVersion = 2
-const CACHE_NAME = 'AquaRooms-v' + cacheVersion 
 
+//Mise en Cache
+const cacheVersion = 2
+const CACHE_NAME = 'AquaRooms-v' + cacheVersion
+//liste des éléments à mettre en cache
 const urlsToCache = [
     '/',
     '/images/192x192.png',
@@ -31,6 +33,8 @@ const urlsToCache = [
 
 ];
 
+const API_URL = 'http://127.0.0.1:5500/index.html';
+
 // ServiceWorker
 self.addEventListener('install', event => {
     event.waitUntil(
@@ -56,8 +60,25 @@ self.addEventListener('activate', event => {
   })
   
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
+    if (event.request.url === API_URL) {
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    if (response.ok) {
+                        const responseClone = response.clone(); 
+                        caches.open(CACHE_NAME)
+                            .then(cache => cache.put(event.request, responseClone));
+                    }
+                    return response;
+                })
+                .catch(() => {
+                    return caches.match(event.request);
+                })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request)
             .then(response => response || fetch(event.request))
-    );
+        );
+    }
 });
